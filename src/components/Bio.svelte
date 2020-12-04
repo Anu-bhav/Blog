23959dbe8019de88594a545d1e0993a53cfb1f13
anu-bhav <myster.anubhav@gmail.com>
2020-12-04 09:24:42 +0400

    bio update

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -22,10 +22,10 @@
   <!-- <img src="profile-pic.png" alt="Maxi Ferreira"> -->
   <p>
     Hi, I'm <strong>Anubhav</strong>. I'm a Software Engineer, Network Engineer and Ethical Hacker from Mauritius.
-    You can follow me on <a href="https://www.instagram.com/_anu_bhav_/" target="_blank">Instagram</a>,
-    You can follow me on <a href="https://www.twitter.com/pr0t0ty03/" target="_blank">Twitter</a>,
-    You can add me on <a href="https://www.facebook.com/anubhav.s.0809/" target="_blank">Facebook</a>,
-    see some of my work on <a href="https://github.com/anu-bhav/" target="_blank">GitHub</a>,
-    or read more about me on <a href="https://anubhav.xyz/" target="_blank">my website</a>.
   </p>
+  <p>You can follow me on <a href="https://www.instagram.com/_anu_bhav_/" target="_blank">Instagram</a></p>,
+  <p>You can follow me on <a href="https://www.twitter.com/pr0t0ty03/" target="_blank">Twitter</a></p>,
+  <p>You can add me on <a href="https://www.facebook.com/anubhav.s.0809/" target="_blank">Facebook</a></p>,
+  <p>See some of my work on <a href="https://github.com/anu-bhav/" target="_blank">GitHub</a></p>,
+  <p>Read more about me on <a href="https://anubhav.xyz/" target="_blank">my website</a></p>.
 </div>
