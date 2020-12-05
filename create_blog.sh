04018f55f5db572c0b0634b0a07af63ad7281c38
anu-bhav <myster.anubhav@gmail.com>
2020-12-05 13:15:55 +0400

    refactored create_blog.sh

--- create_blog.sh
+++ create_blog.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
 function blog(){
-    blog_path="/d/Documents/Development/website_github/Portfolio-Blog/src/routes/blog/posts"
-    static_path="/d/Documents/Development/website_github/Portfolio-Blog/static"
-    backup_dir="/d/Documents/Development/website_github/Posts"
-    FILE="$blog_path"/$1
-    template="$blog_path"/hello-world.md
+    blog_path="/d/Documents/Development/website_github/Portfolio-Blog/src/routes/blog/posts/test"
+    static_path="/d/Documents/Development/website_github/Portfolio-Blog/static/test"
+    backup_dir="/d/Documents/Development/website_github/Posts/test"
+    cd $blog_path || return
+    chmod 777 $blog_path/*
     
-    if [ ! -f $FILE ]; then
-        touch $FILE.md
-        cp $template $FILE.md
-        sed -i "s/title: .*/title: #title#/" $FILE.md
-        sed -i "s/date: \"2019-06-11T08:38:00.000Z\"/date: $(date -u +\"%Y-%m-%d\")/" $FILE.md
-        sed -i "s/Every .*/#description#/" $FILE.md
-        chmod 777 $blog_path/*
-        # cat $FILE.md
-        cd $blog_path
-        $(/d/Program\ Files/Typora/Typora.exe $1.md > /dev/null 2>&1)
+    FILE="$1.md"
+    template="hello-world.md"
+    
+    if [ ! -f "$FILE" ]; then
+        cp $template "$FILE"
+        sed -i "s/title: .*/title: title/" "$FILE"
+        today=$(date -u +"%Y-%m-%d")
+        sed -i "s/date: \"2020-12-02\"/date: $today/" "$FILE"
+        sed -i "s/Every .*/description/" "$FILE"
+        eval "$(/d/Program\ Files/Typora/Typora.exe "$FILE" > /dev/null 2>&1)"
     else
-        cd $blog_path
-        $(/d/Program\ Files/Typora/Typora.exe $1 > /dev/null 2>&1)
+        eval "$(/d/Program\ Files/Typora/Typora.exe "$FILE" > /dev/null 2>&1)"
     fi
     
-    chmod 777 $blog_path/*
-    # if [ -d $(find "$blog_path" ! -path "$blog_path" -type d) ]; then
-    cp -afr $(find "$blog_path" ! -path "$blog_path" -type d) $static_path && echo "Successfully copied" || echo "No such directory created"
-    cp -afr $(find "$blog_path" ! -path "$blog_path" -type d) $backup_dir && echo "Successfully copied" || echo "No such directory created"
-    cp $FILE.md $backup_dir && echo "Successfully copied" || echo "No such file created"
-    rm -rf $(find "$blog_path" ! -path "$blog_path" -type d) && echo "Successfully deleted" || echo "No such directory created"
-    # fi
-    cd - 2>&1
+    cp "$FILE" $backup_dir && echo "Successfully copied $FILE to backup_dir" || echo "No such file created"
+    
+    blog_img_dir=$(find "$blog_path" ! -path "$blog_path" -type d)
+    
+    if [ -d "$blog_img_dir" ]; then
+        cp -afr "$blog_img_dir" $static_path && echo "Successfully copied blog_img_dir to static_path"
+        cp -afr "$blog_img_dir" $backup_dir && echo "Successfully copied blog_img_dir to backup_dir"
+        rm -rf "$blog_img_dir" && echo "Successfully deleted blog_img_dir from blog_path"
+    fi
+    
+    cd - > /dev/null 2>&1 || return
 }
