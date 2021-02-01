1f33d6645f4f50c3466e3e29e42361a6c4bf24ca
anu-bhav <myster.anubhav@gmail.com>
2021-02-01 21:46:01 +0400

    highlight added

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -8,13 +8,13 @@
     width: 180px;
     height: 180px;
     border-radius: 100px;
-    border: 2px solid #63d9fd;
+    border: 3px solid #63d9fd;
     display: block;
     margin-right: 15px;
   }
 
   img:hover {
-    border: 2px solid #a0e9ff;
+    border: 3px solid #a0e9ff;
   }
 
   p {
@@ -25,7 +25,8 @@
 <div>
   <img src="profile-pic.png" alt="Anubhavsingh Sawdagur">
   <p>
-    Hi, I'm <strong>Anubhav</strong>. I'm an Ethical Hacker, Software Engineer and Network Engineer from Mauritius.
+    Hi, I'm <strong class="highlight">Anubhav</strong>. I'm an Ethical Hacker, Software Engineer and Network Engineer
+    from Mauritius.
     <br />
     You can find me on <a href="https://www.instagram.com/_anu_bhav_/" target="_blank">Instagram</a>,
     <a href="https://www.twitter.com/0_Pr0t0typ3/" target="_blank">Twitter</a>,
@@ -34,7 +35,4 @@
     See more of my work on <a href="https://github.com/Anu-bhav?tab=repositories" target="_blank">GitHub</a> or
     read about me on my <a href="https://anubhav.xyz/" target="_blank">Website</a>
   </p>
-  <!-- <p>You can follow me on <a href="https://www.twitter.com/pr0t0ty03/" target="_blank">Twitter</a></p> -->
-  <!-- <p>You can add me on <a href="https://www.facebook.com/anubhav.s.0809/" target="_blank">Facebook</a></p> -->
-  <!-- <p>Read more about me on <a href="https://anubhav.xyz/" target="_blank">my website</a></p> -->
 </div>
