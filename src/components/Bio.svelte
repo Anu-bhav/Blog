6d6242e94cf96ce0e6bdf18018c6251b9b114de1
anu-bhav <myster.anubhav@gmail.com>
2021-02-01 21:12:35 +0400

    added profile picture to bio

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -1,17 +1,17 @@
 <style>
   div {
     align-items: center;
-    /* display: flex; */
+    display: flex;
   }
 
-  /* img {
-    width: 100px;
-    height: 100px;
+  img {
+    width: 180px;
+    height: 180px;
     border-radius: 100px;
-    border: 3px solid #fd6378;
+    border: 2px solid #63d9fd;
     display: block;
-    margin-right: 20px;
-  } */
+    margin-right: 15px;
+  }
 
   p {
     font-size: 1.125rem;
@@ -19,15 +19,15 @@
 </style>
 
 <div>
-  <!-- <img src="profile-pic.png" alt="Maxi Ferreira"> -->
+  <img src="profile-pic.png" alt="Anubhavsingh Sawdagur">
   <p>
     Hi, I'm <strong>Anubhav</strong>. I'm an Ethical Hacker, Software Engineer and Network Engineer from Mauritius.
     <br />
     You can find me on <a href="https://www.instagram.com/_anu_bhav_/" target="_blank">Instagram</a>,
     <a href="https://www.twitter.com/0_Pr0t0typ3/" target="_blank">Twitter</a>,
     <a href="https://www.facebook.com/anubhav.s.0809/" target="_blank">Facebook</a> or
-    <a href="https://www.linkedin.com/in/anubhavsingh-sawdagur-19747b204/" target="_blank">LinkedIn</a>
-    <br />See more of my work on <a href="https://github.com/Anu-bhav?tab=repositories" target="_blank">GitHub</a> or
+    <a href="https://www.linkedin.com/in/anubhavsingh-sawdagur-19747b204/" target="_blank">LinkedIn</a>.
+    See more of my work on <a href="https://github.com/Anu-bhav?tab=repositories" target="_blank">GitHub</a> or
     read about me on my <a href="https://anubhav.xyz/" target="_blank">Website</a>
   </p>
   <!-- <p>You can follow me on <a href="https://www.twitter.com/pr0t0ty03/" target="_blank">Twitter</a></p> -->
