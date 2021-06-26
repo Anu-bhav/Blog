da086694970c7f7a1994fe66bfce065a118479d8
anu-bhav <myster.anubhav@gmail.com>
2021-06-26 15:31:49 +0400

    gh-pages added

---
+++ pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/__sapper__/build`;
+const repoURL = "https://github.com/Anu-bhav/Portfolio-Blog";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "master",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
