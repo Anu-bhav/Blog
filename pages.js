52d52716821a71813785815c5eef24a4acc601a4
anu-bhav <myster.anubhav@gmail.com>
2021-12-01 23:42:13 +0400

    .gitignore updated

--- pages.js
+++ pages.js
@@ -1,12 +1,11 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/__sapper__/export/logbook`;
-const repoURL = "https://github.com/Anu-bhav/logbook";
 
 ghpages.publish(
   pathname,
   {
     branch: "gh-pages",
-    repo: repoURL,
+    repo: "https://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@github.com/Anu-bhav/logbook",
   },
   (err) => {
     if (err) console.log("ERROR: ", err);
